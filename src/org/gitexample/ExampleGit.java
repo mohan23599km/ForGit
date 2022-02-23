6eada694cb0ab27c29c39796889ea8490ed5ce1c
mohan23599km <mohan23599.km@gmail.com>
2022-02-23 14:08:44 +0530

    Finished

---
+++ src/org/gitexample/ExampleGit.java
@@ -0,0 +1,36 @@
+package org.gitexample;
+
+
+
+public class ExampleGit {
+	
+	private void phoneName() {
+		  System.out.println("my phone name is iphone");
+
+		}
+		private void phoneMieiNum() {
+			System.out.println("my phone Miei number is 12345");
+
+		}
+		private void camera() {
+			System.out.println("my camera megapixel is 12mp");
+
+		}
+		private void storage() {
+			System.out.println("my phone storage is 126gb ");
+
+		}
+		private void osName() {
+	        System.out.println("my phone Os name is IOS15");
+
+		}
+		public static void main(String[] args) {
+			ExampleGit ph=new ExampleGit();
+					ph.phoneName();
+					ph.phoneMieiNum();
+					ph.camera();
+					ph.storage();
+					ph.osName();
+		}
+
+}
